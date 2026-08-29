8404554622553ab6ca41f12f63323c945a36ab38
DoganAslan <144552182+DoganAslan@users.noreply.github.com>
2026-08-29 22:04:25 +0300

    Add database access code

---
+++ MagazaOtomasyonu/DataAccess/DatabaseConnection.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace MagazaOtomasyonu.DataAccess
+{
+    internal class DatabaseConnection
+    {
+        public static string connectionString = @"Server=localhost;Database=MagazaOptimizasyonu;Trusted_Connection=True;TrustServerCertificate=True;";
+        
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
