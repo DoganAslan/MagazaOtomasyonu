538b6ddf203bec87eeba69266c1aa231cdd6d815
DoganAslan <144552182+DoganAslan@users.noreply.github.com>
2026-08-29 22:05:48 +0300

    Add session helper

---
+++ MagazaOtomasyonu/Helpers/UserSession.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagazaOtomasyonu.Helpers
+{
+    internal static class UserSession
+    {
+        public static int UserId { get;  set; }
+        public static string Username { get; set; } = string.Empty;
+        public static string FirstName { get; set; } = string.Empty;
+        public static string LastName { get; set; } = string.Empty;
+        public static string RoleName { get; set; } = string.Empty;
+    }
+}
